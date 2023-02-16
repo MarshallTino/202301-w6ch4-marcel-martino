4925d2beeff9377a00bb3e3d7c1970076c8ec7c2
Marcel Martino <marcelmartino2053@gmail.com>
2023-02-16 19:58:41 +0100

    Create types create thingsthatiknow array

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-import "./LoadEnv";
+import "./LoadEnv.js";
 import morgan from "morgan";
 import express from "express";
+import createDebugMessage from "debug";
+import thingsThatiKnow from "./things.js";
+
+const port = process.env.PORT;
+const debug = createDebugMessage("app");
 
 const app = express();
+app.use((req, res, next) => {
+  console.log("Gholadsa");
+  debug("app");
+  next();
+});
+
+app.get("/things", (req, res) => {
+  console.log("a request to / has been received");
+  res.json(thingsThatiKnow);
+});
 
 app.use(morgan("dev"));
+debug("app");
+
+app.listen(port);
