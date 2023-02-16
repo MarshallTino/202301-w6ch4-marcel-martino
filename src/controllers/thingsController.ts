7cc38dfa8701e21624f18ddb921e0b1d6ae653d1
Marcel Martino <marcelmartino2053@gmail.com>
2023-02-16 22:49:19 +0100

    Add delete method add status codes and error manegament

--- src/controllers/thingsController.ts
+++ src/controllers/thingsController.ts
@@ -1,12 +1,28 @@
-import { type NextFunction, type Request, type Response } from "express";
+import debug from "debug";
+import { type Request, type Response } from "express";
 import thingsThatiKnow from "../things.js";
 
-export const getThing = (req: Request, res: Response, next: NextFunction) => {
+export const getThing = (req: Request, res: Response) => {
   const { id } = req.params;
   const thing = thingsThatiKnow.find((thing) => thing.id === +id);
-  res.json({ thing });
+  if (!thing) {
+    res.status(404).json({ error: "No Thing found" });
+  }
+
+  res.status(200).json({ thing });
 };
 
-export const getThings = (req: Request, res: Response, next: NextFunction) => {
-  res.json(thingsThatiKnow);
+export const getThings = (req: Request, res: Response) => {
+  res.status(200).json(thingsThatiKnow);
+};
+
+export const deleteThing = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const thing = thingsThatiKnow.find((thing) => thing.id === +id);
+  thingsThatiKnow.splice(0, thing?.id);
+  if (!thing) {
+    res.status(404).json({ error: "No Thing found" });
+  }
+
+  res.status(200).json(thingsThatiKnow);
 };
